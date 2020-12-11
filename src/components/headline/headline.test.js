c5e126d8a07470602722773f320a08d493b5137b
Ashutosh <ashutosh_tripathi@ext.dunzo.in>
2020-12-11 16:41:23 +0530

    add redux to our app

--- src/components/headline/headline.test.js
+++ src/components/headline/headline.test.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { shallow } from "enzyme"
-import checkPropTypes from 'check-prop-types';
 
 import { findByTestAttr, checkProps } from "../../../utils"
 import Headline from "./index"
